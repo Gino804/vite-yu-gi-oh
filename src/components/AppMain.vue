8b0740a85faa668a890918335a6415301750659c
Gino804 <giacomo.p03@gmail.com>
2023-07-03 15:47:28 +0200

    Added AppMain

---
+++ src/components/AppMain.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <div class="frame">
+            <div class="screen"></div>
+        </div>
+    </div>
+</template>
+
+<style>
+.frame {
+    background-color: #DEDEDE;
+    padding: 50px 20px;
+    margin-top: 150px;
+    border-radius: 30px;
+}
+
+.screen {
+    background-color: #666A6F;
+    padding: 20px 10px;
+}
+</style>
