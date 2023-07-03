1c6a3cd1eee64aba88254797a4e5afa6abd2eb59
Gino804 <giacomo.p03@gmail.com>
2023-07-03 16:43:57 +0200

    Added card background

--- src/components/PokemonCard.vue
+++ src/components/PokemonCard.vue
@@ -5,12 +5,24 @@ export default {
         number: Number,
         name: String,
         type: String
+    },
+    methods: {
+        getBgClass() {
+            let bg = '';
+            const type = this.type;
+
+            if (type === 'Grass') bg = 'bg-grass';
+            else if (type === 'Fire') bg = 'bg-fire';
+            else if (type === 'Water') bg = 'bg-water';
+
+            return bg;
+        }
     }
 }
 </script>
 
 <template>
-    <div class="card">
+    <div class="card" :class="getBgClass()">
         <img :src="image" :alt="name">
         <p>{{ number }}</p>
         <h4>{{ name }}</h4>
@@ -18,9 +30,8 @@ export default {
     </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 .card {
-    background-color: white;
     border-radius: 15px;
     text-align: center;
     padding: 15px 0;
