2cffd9d668164f6e715b1e8d288cdb984aba709a
Gino804 <giacomo.p03@gmail.com>
2023-07-03 15:33:08 +0200

    Added base structure

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,26 @@
 <script>
 </script>
 
-<template></template>
+<template>
+  <div class="container">
+    <div class="frame">
+      <div class="screen"></div>
+    </div>
+  </div>
+</template>
 
-<style></style>
+<style lang="scss">
+@use './assets/scss/style.scss';
+
+.frame {
+  background-color: #DEDEDE;
+  padding: 50px 20px;
+  margin-top: 150px;
+  border-radius: 30px;
+}
+
+.screen {
+  background-color: #666A6F;
+  padding: 20px 10px;
+}
+</style>
